a4ab3f37a514a362e09d85b1d84922fa2504e62c
user.email <Kevin.danielm06@gmail.com>
2022-02-13 13:08:58 +0100

    Router, RouterLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,27 @@ import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, MonPremierComponent, AppareilComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [AppareilService],
+  declarations: [
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  providers: [AppareilService, AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
